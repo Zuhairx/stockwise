8d4b54ff17ee854f5468005c7babf57406bb7049
m. zuhair .y <zuhairyuliansyah688@gmail.com>
2026-01-12 14:32:14 +0700

    update 4 final

--- run.bat
+++ run.bat
@@ -1,4 +1,11 @@
 @echo off
+echo Redeploying StockWise Inventory Management System...
+mvn clean package
+if %errorlevel% neq 0 (
+    echo Build failed!
+    pause
+    exit /b 1
+)
 echo Starting StockWise Inventory Management System...
 java --module-path target\lib --add-modules javafx.controls,javafx.fxml -jar target\stockwise-1.0-SNAPSHOT-shaded.jar
 pause
